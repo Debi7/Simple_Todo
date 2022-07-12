8f2a05a73d6193c64925cb77152e596faa579e0c
Debi7 <debi1@mail.ru>
2022-07-12 16:35:19 +0300

    new

--- my_note.js
+++
@@ -1,9 +0,0 @@
-// создаем новый элемент с тегом li
-const element = document.createElement("li");
-// добавляем ему класс
-element.classList.add("text");
-// запихиваем внутрь его какой то контент
-element.innerHTML = "Это контент";
-
-// добавляем в конец контейнера
-container.appendChild(element);
